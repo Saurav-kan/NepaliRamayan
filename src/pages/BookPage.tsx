ca8b376f7ca82dec581aa5dd1b81b52ea880f3e5
Saurav Kandel <sauravk0633@gmail.com>
2026-05-03 01:11:27 -0500

    title

--- src/pages/BookPage.tsx
+++ src/pages/BookPage.tsx
@@ -1,5 +1,13 @@
 import { BookReader } from '../components/BookReader'
 
 export function BookPage() {
-  return <BookReader />
+  return (
+    <>
+      <header className="book-header">
+        <p className="book-header-line book-header-line--ne">रामचरितमानस</p>
+        <p className="book-header-line book-header-line--en">Ram Charitmanas</p>
+      </header>
+      <BookReader />
+    </>
+  )
 }
